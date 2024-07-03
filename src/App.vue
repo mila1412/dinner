d6dc2e22afd15613d4b1cdcc74353fe04d097308
milawu <milawu@urtoday.com>
2024-07-03 14:12:40 +0800

    🌈 style: add style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <div v-if="!allFood.length" class="title">吃啥</div>
-    <ul v-else class="wrap">
+  <div class="wrap">
+    <div v-if="showTitle" class="title">吃啥</div>
+    <ul class="list">
       <li
         v-for="(food, index) in allFood"
         :key="index"
@@ -13,7 +13,8 @@
       </li>
     </ul>
   </div>
-  <button class="start" :disabled="playing" @click="start">start</button>
+  <button v-if="!playing" class="btn-start" @click="start">Click Me</button>
+  <button v-else class="btn-start" disabled>Choosing...</button>
 </template>
 
 <script setup>
@@ -21,10 +22,12 @@ import { ref } from 'vue';
 import axios from 'axios';
 const allFood = ref([]);
 const playing = ref(false);
+const showTitle = ref(true);
 
 async function start() {
   allFood.value = [];
   playing.value = true;
+  showTitle.value = false;
   const sheetID = import.meta.env.VITE_SHEET_ID;
   const apiKey = import.meta.env.VITE_API_KEY;
   const res = await axios.get(
@@ -50,34 +53,67 @@ function restart() {
 
 <style lang="scss">
 .title {
-  line-height: 30px;
-  height: 30px;
+  color: #fff;
 }
+
 .wrap {
-  position: relative;
+  box-sizing: border-box;
+  width: 200px;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: rgb(139, 23, 23);
+  border: 2px solid rgb(83, 22, 22);
+}
+
+.list {
+  width: 200px;
   overflow: hidden;
   max-height: 30px;
   padding: 0;
   margin: 0;
 }
+
 .item {
-  list-style: none;
-  line-height: 30px;
-  height: 30px;
   position: relative;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  list-style: none;
+  color: #fff;
 }
+
 .playing {
   animation-name: random;
   animation-iteration-count: 2;
   animation-timing-function: linear;
   animation-duration: 0.5s;
 }
+
 @keyframes random {
   from {
     top: 0;
   }
   to {
     top: -200px;
+  }
+}
+
+.btn-start {
+  width: 200px;
+  height: 50px;
+  color: rgb(83, 22, 22);
+  font-size: 16px;
+  font-weight: bold;
+  background: rgb(226, 196, 27);
+  border: 2px solid rgb(83, 22, 22);
+  border-top: none;
+  cursor: pointer;
+  &:focus {
+    background: rgb(83, 22, 22);
+    color: #fff;
   }
 }
 </style>
